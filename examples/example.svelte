2ef5b6d9f62eb95ed28496b8afe8bf571c5134bc
midudev <miduga@gmail.com>
2025-12-09 18:45:28 +0100

    feat: Update star icon SVG with improved attributes and accessibility features

--- examples/example.svelte
+++ examples/example.svelte
@@ -2,6 +2,21 @@
   // Svelte component
 </script>
 
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-  <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon>
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  id="my-star-icon"
+  class="icon-star"
+  width="24"
+  height="24"
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="currentColor"
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+  data-testid="star-icon"
+>
+  <polygon
+    points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"
+  ></polygon>
 </svg>
